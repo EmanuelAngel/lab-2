4dd7c3f1acaab94bc80d054196639add80be010d
EmanuelAngel <emanuelangelsbr@gmail.com>
2025-02-03 21:31:46 -0300

    refactor: move CSS and JS imports to main files

---
+++ public/js/main.js
@@ -0,0 +1,12 @@
+// jQuery 3.7.1
+import '../../../lib/jQuery/jquery-3.7.1.min.js'
+// Popper 2.11.8 (for Bootstrap 5.3.3)
+import '../../../lib/popper/core/dist/umd/popper.min.js'
+// Bootstrap 5.3.3
+import '../../../lib/bootstrap/js/dist/js/bootstrap.bundle.min.js'
+// DataTables 2.1.8
+import '../../../lib/dataTables/datatables.min.js'
+
+// Custom JS
+import './initTable.js'
+import './initTooltips.js'
